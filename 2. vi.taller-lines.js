e64dfb41176786ea9f58a6b03e97aa34ee29e0d7
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-02-04 15:42:22 -0800

    Create 2. vi.taller-lines.js

---
+++ 2. vi.taller-lines.js
@@ -0,0 +1,14 @@
+var x1= 5;
+var x2 =10;
+
+while(x1<=340 && x2<=340){
+	line(x1,0,x1,x2);
+	x1= x1+5;
+x2= x2+15;
+}
+
+var counter = 105;
+while(counter<=340){
+	line(counter,0,counter,340);
+	counter = counter+5;
+}
